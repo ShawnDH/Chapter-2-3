e776542fa5e486ecf2a50c7d97ba4189cb45a35c
ShawnDH <37811310+ShawnDH@users.noreply.github.com>
2018-04-05 12:37:12 -0500

    update PayCheck

---
+++ PayCheck/PayCheck/Source.cpp
@@ -0,0 +1,25 @@
+//Paycheck Calculator
+//04/05/2018
+//Author: ShawnD Herrick
+
+
+#include <iostream>
+
+
+int main()
+{
+	double hoursWorked;
+	double hourlyPay;
+	int netPaid;
+	netPaid = hourlyPay * hoursWorked;
+
+	std::cout << "Please input your hourly pay \n >";
+	std::cin >> hourlyPay;
+	std::cout << "Please input the hours you worked \n >";
+	std::cin >> hoursWorked;
+	std::cout << "Your net pay is >>>" << netPaid << std::endl;
+
+
+	system("Paused");
+	return 0;
+}
