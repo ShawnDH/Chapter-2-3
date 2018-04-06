3a20640959e90c893272b80a0c094b7f86e70b72
ShawnDH <37811310+ShawnDH@users.noreply.github.com>
2018-04-06 13:12:16 -0500

    update Lake View Hotel

---
+++ Lake View Hotel/Lake View Hotel/Source.cpp
@@ -0,0 +1,40 @@
+/*Lake View Hotel Bill Calculator
+04/06/2018
+Author: ShawnD Herrick */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int nightsStayed;
+	int perNightRate = 100;
+	int roomServiceYoN;
+	int oneTimePhoneYoN;
+	int phoneMin;
+	int roomService = 35;
+	double phoneCharge = 0.25;
+
+	std::cout << "Welcome to the Lake View Hotel! \n How many nights would you like to stay? ($100 per night) \n >";
+	std::cin >> nightsStayed;
+
+	std::cout << "Would you like room service?  ($35 one time fee) \n 1.Yes  or  2.No \n > #";
+	std::cin >> roomServiceYoN;
+
+	std::cout << "Would you like to use our one-time telephone? \n 1.Yes  or  2.No \n > #";
+	std::cin >> oneTimePhoneYoN;
+	if (oneTimePhoneYoN == 1)
+	{
+		std::cout << "In minuites, how long would you like to use the phone? ($0.25 per minuite) \n >";
+		std::cin >> phoneMin;
+	}
+	
+	std::cout << "Your total is $" << (nightsStayed * perNightRate) + (roomServiceYoN * roomService) + (phoneMin * 0.25) << std::endl;
+
+
+
+
+
+	system("pause");
+	return 0;
+}
